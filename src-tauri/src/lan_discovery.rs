d6eda234b7015b985ec4dd82d0c1bb1db1edec85
Nciibi <ncibia77@gmail.com>
2026-06-30 16:28:51 +0100

    v3.2.157

--- src-tauri/src/lan_discovery.rs
+++ src-tauri/src/lan_discovery.rs
@@ -249,8 +249,8 @@ pub async fn start(
         .map_err(LanDiscoveryError::Io)?;
 
     // Join the multicast group
-    socket.join_multicast_v4(&MULTICAST_ADDR, &Ipv4Addr::UNSPECIFIED)
-        .map_err(LanDiscoveryError::Io)?;
+    let _ = socket.join_multicast_v4(&MULTICAST_ADDR, &Ipv4Addr::UNSPECIFIED)
+        .map_err(LanDiscoveryError::Io);
 
     let socket = Arc::new(socket);
     let socket_listener = socket.clone();
